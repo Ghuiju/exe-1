31b39fb0c64f1824603c8b5d3e23e8a5a2d61b8d
Ghuiju <guilhermecesar.demarchi@hotmail.com>
2025-03-18 20:37:05 -0300

    aula 18/03

---
+++ ex/ex4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	float R;
+	
+	puts("Calculadora de Circulo");
+	puts("");
+	
+	printf("Digite o raio: "); scanf("%f", %R);
+	
+	prinf("Diametro = %.2f\n", R * 2);
+	prinf("Diametro = %.2f\n", R * 2);
+}
